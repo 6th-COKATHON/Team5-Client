6d12ad16892a9bf9f363652de7bded767d99e6e8
kimminna <kmina121777@naver.com>
2025-07-26 02:01:08 +0900

    feat: 케미 페이지 레이아웃 및 라우팅 설정

---
+++ src/layout/ChemistryLayout.tsx
@@ -0,0 +1,18 @@
+import {Outlet} from 'react-router-dom';
+
+const ChemistryLayout = () => {
+  return (
+    <div className='w-full min-h-screen flex justify-center overflow-x-hidden'>
+      <div
+        className='max-w-[540px] w-full shadow-2xl px-20 mx-auto'
+        style={{
+          backgroundImage:
+            'linear-gradient(0deg, var(--pink3, #FCC9E0) 0%, #FFF 99.99%, #FFF 100%)',
+        }}>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default ChemistryLayout;
