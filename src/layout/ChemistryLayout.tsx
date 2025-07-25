a944eb40a8cc8a61ca80d17be8810eaec9894911
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-07-26 05:19:34 +0900

    feat: chemistryresultpage ui 수정

--- src/layout/ChemistryLayout.tsx
+++ src/layout/ChemistryLayout.tsx
@@ -4,7 +4,7 @@ const ChemistryLayout = () => {
   return (
     <div className='w-full min-h-screen flex justify-center overflow-x-hidden'>
       <div
-        className='max-w-[540px] w-full shadow-2xl px-20 mx-auto'
+        className='max-w-[540px] w-full shadow-2xl mx-auto'
         style={{
           backgroundImage:
             'linear-gradient(0deg, var(--pink3, #FCC9E0) 0%, #FFF 99.99%, #FFF 100%)',
