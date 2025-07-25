50384f979ba688801ac843a856cf46b61e62fe0e
kimminna <kmina121777@naver.com>
2025-07-26 04:08:38 +0900

    feat: 온보딩 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import TestFlowPage from './pages/TestFlowPage';
 import ChemistryLayout from './layout/ChemistryLayout';
 import ChemistryPage from './pages/ChemistryPage';
 import CreateChemistryPage from './pages/CreateChemistryPage';
+import IntroPage from './pages/IntroPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<TestLayout />}>
-          <Route index element={<TestFlowPage />}></Route>
-          <Route path='/create' element={<CreateChemistryPage />}></Route>
+          <Route index element={<IntroPage />}></Route>
+          <Route path='test' element={<TestFlowPage />}></Route>
+          <Route path='create' element={<CreateChemistryPage />}></Route>
         </Route>
         <Route element={<ChemistryLayout />}>
           <Route path='/chemistry' element={<ChemistryPage />}></Route>
