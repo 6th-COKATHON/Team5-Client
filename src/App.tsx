b2d7b57466318054704a2a99ffc0cacd974eaf07
kimminna <kmina121777@naver.com>
2025-07-26 02:38:36 +0900

    feat: 케미스트리 생성 페이지 UI 및 라우팅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import TestLayout from './layout/TestLayout';
 import TestFlowPage from './pages/TestFlowPage';
 import ChemistryLayout from './layout/ChemistryLayout';
 import ChemistryPage from './pages/ChemistryPage';
+import CreateChemistryPage from './pages/CreateChemistryPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<TestLayout />}>
-          <Route path='/' element={<TestFlowPage />}></Route>
+          <Route index element={<TestFlowPage />}></Route>
+          <Route path='/create' element={<CreateChemistryPage />}></Route>
         </Route>
         <Route element={<ChemistryLayout />}>
           <Route path='/chemistry' element={<ChemistryPage />}></Route>
