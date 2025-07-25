9e533a8a16c34b9a014e3fbb5bffa5cea61d11f2
kimminna <kmina121777@naver.com>
2025-07-26 02:39:00 +0900

    style: variant 추가

--- src/components/OptionItem.tsx
+++ src/components/OptionItem.tsx
@@ -1,10 +1,25 @@
 interface OptionItemProps {
   text: string;
+  variant?: 'text-light' | 'text-bold';
+  onClick: () => void;
+  isSelected: boolean;
 }
 
-const OptionItem = ({text}: OptionItemProps) => {
+const OptionItem = ({
+  text,
+  variant = 'text-light',
+  onClick,
+  isSelected,
+}: OptionItemProps) => {
   return (
-    <div className='w-[438px] h-[60px] flex justify-center items-center text-xl font-semibold rounded-[50px] border-2 border-dashed border-pink-400 bg-transparent'>
+    <div
+      onClick={onClick}
+      className={`
+        w-[438px] h-[60px] flex justify-center items-center text-xl rounded-[50px] cursor-pointer
+        transition-all duration-200 border-2 border-dashed border-primary-pink
+        ${variant === 'text-light' ? 'font-normal' : 'font-semibold'}
+        ${isSelected ? ' bg-pink-1 text-white' : ' bg-transparent text-black'}
+      `}>
       {text}
     </div>
   );
